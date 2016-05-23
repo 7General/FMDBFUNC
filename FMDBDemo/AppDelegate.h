0f7f740f162e6043aded798c3e096b95357e70f8
王会洲 <wanghuizhou21@163.com>
2016-05-23 14:53:29 +0800

    上传基本工程框架

---
+++ FMDBDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBDemo
+//
+//  Created by 王会洲 on 16/5/23.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMDBDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDBDemo
+//
+//  Created by 王会洲 on 16/5/23.
+//  Copyright © 2016年 王会洲. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
